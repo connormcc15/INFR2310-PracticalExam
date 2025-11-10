957f52df1f07c4794dfef1c95e8894ebbd5d27fd
connormcc15 <connor.mccarthy3@ontariotechu.net>
2025-11-10 17:59:07 -0500

    Reimported exam resources

--- PracticalExamINFR2310/Assets/RayCaster.cs
+++ PracticalExamINFR2310/Assets/RayCaster.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class RayCaster : MonoBehaviour
 {
 
-    private Camera cam;//Camera Object
+ 
+    private Camera cam;
     void Start()
     {
         cam = Camera.main;
@@ -11,7 +12,6 @@ public class RayCaster : MonoBehaviour
 
     void Update()
     {
-        //If the mouse is clicked, the camera moves to that position
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -20,8 +20,7 @@ public class RayCaster : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.point);  //point is a position x,y,z  e.g. this.transform.position = hit.point
-            }
+            }       
         }
     }
 }
-
